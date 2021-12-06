8efcd7c1b3e4fb33440f84792de0b8073488bdf6
katiehons <katiebug@spu.edu>
2021-12-06 12:43:07 -0800

    modified App.js and added blank files for all app pages

---
+++ public/electron.js
@@ -0,0 +1,127 @@
+// NOTE: the analogous file is src/main.js in the original
+const path = require('path');
+
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  win.loadURL(
+//TODO don't use the conditional operator. please.
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+
+
+
+
+/// alll the code from v1
+// const { app, BrowserWindow } = require('electron')
+// // const { ipcMain } = require('electron')
+// const path = require('path');
+// // const Store = require('electron-store');
+// // const DEBUG = true;
+//
+// // require('./db_connect/main');
+//
+// const createWindow = () => {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true,
+//       contextIntegration: false
+//     }
+//   })
+//   win.loadURL('http://localhost:3000/')
+// }
+//
+// app.whenReady().then(createWindow)
+//
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
+//
+// app.on('activate', () => {
+//   if (BrowserWindow.getAllWindows().length === 0) {
+//     createWindow()
+//   }
+// })
+//
+//
+// // user data management
+// const schema = {
+// 	current_user: {
+// 		type: 'string',
+// 		default: 'guest'
+// 	},
+//   library_setup: {
+//     type: 'boolean',
+//     default: false
+//   },
+//   library_name: {
+//     type: 'string',
+//     default: ""
+//   }
+// };
+//
+// var userDataPath = app.getPath('userData');
+//
+// const library_state = new Store({ schema });
+//
+// // Retrieves data of current profile
+// ipcMain.handle('getStoreValue', (event, key) => {
+//     if (DEBUG === true){
+//       console.log("Library state for " + key + " requested: " + library_state.get(key));
+//       console.log("PATH: " + app.getPath('userData'));
+//     }
+//     return library_state.get(key);
+// });
+//
+// // Sets data for current profile
+// ipcMain.handle('setStoreValue', (event, key, value) => {
+//   if (DEBUG === true) {
+//     console.log("Library state for " + key + " is now " + value);
+//     console.log("PATH: " + app.getPath('userData'));
+//   }
+//     return library_state.set(key, value);
+// });
--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/NavbarFiles/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Profiles';
+import Setup from './Pages/Setup';
+import Search from './Pages/Search';
+import AddProfile from './Pages/AddProfile';
+import AddBookManually from './Pages/AddBookManually';
+import AddBookAPI from './Pages/AddBookAPI';
+import MetadataEdit from './Pages/MetadataEdit';
+
+import { useState } from 'react';
+// Used for conditional setup
+const electron = window.require('electron');
+const { ipcRenderer } = electron;
+var firstPage;
 
 function App() {
+  // If the PLOT software has not been set up locally, then the setup page is routed to by default.
+  // Otherwise, the Login page will greet the user.
+  const [isSetup, setIsSetup] = useState([]);
+
+  const checkSetup = async () => {
+    const setupState = await ipcRenderer.invoke('getStoreValue','library_setup');
+    console.log("library is setup: " + setupState);
+    setIsSetup( setupState );
+  };
+
+  if(isSetup.length === 0) {
+    setIsSetup(false);
+    checkSetup();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <>
+      <Router>
+        <Switch>
+        <Route path='/' exact component={((isSetup) ? Login : Setup)} />
+          <Route path='/Login' exact component={Login} />
+          <Route path='/Setup' exact component={Setup} />
+          <Route path='/AddProfile' exact component={AddProfile} />
+          <div>
+          <Navbar />
+          <Route path='/Home' exact component={Home} />
+          <Route path='/Search' exact component={Search} />
+          <Route path='/AddBookAPI' exact component={AddBookAPI} />
+          <Route path='/AddBookManually' exact component={AddBookManually} />
+          <Route path='/MetadataEdit' exact component={MetadataEdit} />
+          </div>
+        </Switch>
+      </Router>
+    // </>
   );
 }
 
 export default App;
+// template code:
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
