e4cffc9fcdc13b1a28b66efaad3e20acdaa62b1a
katiehons <katiebug@spu.edu>
2022-04-12 22:13:09 -0700

    profiles page loads users from sequelize

--- src/pages/profiles.js
+++ src/pages/profiles.js
@@ -1,133 +1,96 @@
 import {React, useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import sendAsync from '../db_connect/renderer';
 
 import { Sequelize } from 'sequelize';
-// import User from '../db_connect/sequelize_index';
-// const { User } = require( '../db_connect/sequelize');
 const electron = window.require('electron');
 const { ipcRenderer } = electron;
 const electron_store = window.require('electron-store');
 const { store } = electron_store;
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './data/library.db',
-  define: {
-    timestamps: false
-  }
-});
-
-async function catch_err(){
-  try {
-    await sequelize.authenticate();
-    console.log('sequelize Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the sequelize database:', error);
-  }
-}
-
-catch_err();
-
-const User = require('../db_connect/models/user')(sequelize)
-
-User.findAll().then((users) => {
-  console.log(users.every(user => user instanceof User)); // true
-  console.log("(profiles)All users:", JSON.stringify(users, null, 2));
-  // console.log(users)
-  sequelize.close()
-})
-
-// async function get_users()
-// {
-//     try{
-//     const users = await User.findAll();
-//     console.log(users.every(user => user instanceof User)); // true
-//     console.log("All users:", JSON.stringify(users, null, 2));
-//     } catch (error) {
-//     console.error('Users table err:', error);
-//     }
-//     // const users = await User.findAll();
-//   // console.log(users.every(user => user instanceof User)); // true
-//   // console.log("All users:", JSON.stringify(users, null, 2));
-// }
-
 function Login() {
-  // sequelize explorations
-
-
-  /// fin sequelize explaorations
-
   let history = useNavigate();
 
-  const setUser = (username) => {
-    console.log("profil click! for user " + username);
-    console.log('profile is ' + ipcRenderer.invoke('getStoreValue', 'current_user'));
-    ipcRenderer.invoke('setStoreValue', 'current_user', username);
-    console.log('now profile is ' + ipcRenderer.invoke('getStoreValue', 'current_user'));
+  // users and library name
+  const [profiles, setProfiles] = useState([]);
+  const [libName, setLibName] = useState([]);
 
+  // Set user on profile button click
+  const setUser = (username) => {
+    // console.log("profil click! for user " + username);
+    // console.log('profile is ' + ipcRenderer.invoke('getStoreValue', 'current_user'));
+    ipcRenderer.invoke('setStoreValue', 'current_user', username);
+    // console.log('now profile is ' + ipcRenderer.invoke('getStoreValue', 'current_user'));
+
+    // close db connection for this page before home
+    sequelize.close();
     history("/Home");
-    // library_state.set('current_user', 'Me');
   }
 
+  // username button
   function makeButton(user) {
-    console.log('button for ' + user);
+    // console.log('button for ' + user);
     return (
       <button className="profiles-button" onClick={() => setUser(user)}>{user}</button>
     )
   }
 
+  // all username buttons
   function ProfileButtons({ profiles }) {
-    console.log("making *all* the buttons");
-    console.log(profiles);
+    // console.log("making *all* the buttons");
+    // console.log(profiles);
     return (
       <span id='generated-profilesButtons'>{profiles.map((user) => makeButton(user))}</span>
     )
   }
 
-  const [profiles, setProfiles] = useState([]);
-  const [libName, setLibName] = useState([]);
-  // TODO read in the actual library Name
-  // TODO add generated list of profile buttons from stored user profiles'
-  // console.log("config.json at " + app.getPath('userData'));
-  // console.log(app.getPath('userData'));
-  //
+  // get library from electron store (settings and config info)
+  if (libName.length === 0){
+    ipcRenderer.invoke('getStoreValue', 'library_name').then((result) => {
+      // console.log("library name: " + result);
+      setLibName(result);
+    })
+  }
 
   function LibraryHeader({ name }) {
-    console.log("getting library name");
-    console.log(name);
+    // console.log("getting library name");
+    // console.log(name);
     return (
       <h1 id='library_name'>The {name} Library</h1>
     )
   }
 
-  if (libName.length === 0){
-    ipcRenderer.invoke('getStoreValue', 'library_name').then((result) => {
-      // library_name = result;
-      console.log("library name: " + result);
-      setLibName(result);
-    })
-  }
+  // Get usernames from database
+  const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './data/library.db',
+    define: {
+      timestamps: false
+    }
+  });
+
+  (async function(){
+    try {
+      await sequelize.authenticate();
+      console.log('sequelize Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the sequelize database:', error);
+    }
+  })();
+
+  const User = require('../db_connect/models/user')(sequelize)
 
   if (profiles.length === 0) {
-    console.log("generating login pg…");
-    var sql_get_users = "SELECT username FROM users";
-    sendAsync(sql_get_users).then((result) => {
-      console.log("our users:")
-      console.log(result);
-
-      // var current_profiles = document.getElementById('profileButtons');
-
-      var users = [];
-      for (var i = 0; i < result.length; i++) {
-        users.push(result[i].username);
+    User.findAll().then((users) => {
+      console.log(users.every(user => user instanceof User)); // true
+      console.log("(profiles)All users:", JSON.stringify(users, null, 2));
+      console.log(users)
+      var usernames = [];
+      for (var i = 0; i < users.length; i++) {
+        usernames.push(users[i].username);
       }
-      console.log(users);
-
-      if (users.length > 0)
-      {
-        setProfiles(users);
-      }
+      console.log("sequelize got simplified users")
+      console.log(usernames);
+      setProfiles( usernames );
     });
   }
 
@@ -144,9 +107,6 @@ function Login() {
       </div>
     </div>
   )
-  // return(
-  //   <h1>profiles</h1>
-  // )
 }
 
     export default Login
